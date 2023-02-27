1b71d008e041cebdf5ab578a341f7d47068721e5
core.editor <jhhoyosb@gmail.com>
2023-02-27 11:59:24 -0500

    God4

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('../source'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.mathjax']
+extensions = ['sphinx.ext.autodoc','myst_parser']
 myst_enable_extensions = [
     "amsmath",
     "attrs_inline",
